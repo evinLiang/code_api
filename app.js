7ee85a5d977febc4afa5cbf94bce5a8b53a0d612
evinLiang <evin@evinweb.com>
2018-09-17 12:02:07 +0800

    设置跨域

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const autoController = require('express-json-server');
 
 const app = express();
 
+//设置跨域访问
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+
 //引入、使用路由
 var indexRouter = require('./routes/index');
 app.use('/', indexRouter);
